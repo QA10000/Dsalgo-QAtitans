71942cde2a13f012939740ad60a0e262f268c5a9
nishi kalra <nbehlqa@gmail.com>
2025-08-13 17:15:27 -0500

    Created stepdefinition for register page

---
+++ src/main/java/com/qa/dsalgo/pages/Background.java
@@ -0,0 +1,28 @@
+package com.qa.dsalgo.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Background {
+	private WebDriver driver;
+
+	public Background(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+	
+	@FindBy(linkText = "Get Started")
+	WebElement getStartedBtn; // Adjust if your structure differs
+	public void ClickGetStarted() {
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    wait.until(ExpectedConditions.elementToBeClickable(getStartedBtn));
+		getStartedBtn.click();
+	}
+}
--- src/test/java/com/qa/dsalgo/runner/TestRunner.java
+++ src/test/java/com/qa/dsalgo/runner/TestRunner.java
@@ -1,5 +1,14 @@
 package com.qa.dsalgo.runner;
 
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+	    features = "src/test/resources/features/register.feature",
+	    glue = {"com.qa.dsalgo.stepdefinitions"},
+	    dryRun= true,
+	    plugin= {"pretty"}
+	)
+
 public class TestRunner {
 
 }
