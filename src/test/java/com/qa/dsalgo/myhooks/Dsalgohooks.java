1b83d6095a0aef27b8c126e5eb4b0d979c7b7f21
nishi kalra <nbehlqa@gmail.com>
2025-08-19 10:20:57 -0500

    Added missing files

---
+++ src/test/java/com/qa/dsalgo/myhooks/Dsalgohooks.java
@@ -0,0 +1,21 @@
+package com.qa.dsalgo.myhooks;
+
+import io.cucumber.java.Before;
+
+import com.qa.dsalgo.base.DriverScripts;
+
+import io.cucumber.java.After;
+
+public class Dsalgohooks extends DriverScripts {
+	@Before
+	public void setup() {
+		System.out.println("This is good");
+	}
+	
+	@After
+	public void tearDown(){
+		closeCurrentWindow();		
+	}
+
+}
+
