bbdde45d3347e5a439bf6929f10e128941ee4057
nishi kalra <nbehlqa@gmail.com>
2025-09-17 20:41:27 -0500

    WIP: Temporary commit before resolving PR conflicts

--- src/test/java/com/qa/dsalgo/stepdefinitions/GraphSteps.java
+++ src/test/java/com/qa/dsalgo/stepdefinitions/GraphSteps.java
@@ -1,25 +1,53 @@
 package com.qa.dsalgo.stepdefinitions;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import com.qa.dsalgo.pages.Background;
+import com.qa.dsalgo.pages.GraphPage;
+
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GraphSteps {
+	private WebDriver driver;
+	GraphPage graphpage;
+	Background background;
 	
-	@Given("The user is on the Home page after Sign in")
-	public void the_user_is_on_the_home_page_after_sign_in() {
+	public GraphSteps() {
+		driver = Hooks.getDriver();
+		background = new Background(driver);
+		graphpage = new GraphPage(driver);
 	}
 
-	@When("The user clicks the {string} button in Graph Panel")
-	public void the_user_clicks_the_button_in_graph_panel(String string) {
-	}
 
+
+	@When("user clicks {string} button in Graph Panel")
+	public void user_clicks_button_in_graph_panel(String string) {
+		graphpage.clickGraphStrtBtn();
+
+	   
+	}
 	@Then("The user be directed to {string} Data Structure Page")
-	public void the_user_be_directed_to_data_structure_page(String string) {
+	public void the_user_be_directed_to_data_structure_page(String expectedTitle) {
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle, "User did not land on the expected page");
+
 	}
+	
+	@Given("The user is on Graph Data Structure")
+	public void the_user_is_on_graph_data_structure() {
+		graphpage.clickGraphStrtBtn();
+		Assert.assertEquals(graphpage.getGraphPageTitle(), "Graph", "Not on Graph page!");
+	}
+
 
 	@When("The user select Graph item from the drop down")
-	public void the_user_select_graph_item_from_the_drop_down() {
+	public void the_user_select_graph_item_from_the_drop_down(String OptionOnGraph) {
+		graphpage.clickGraphOptions(OptionOnGraph);
+
 	}
 
 	@Given("The user is in the Assessments Page")
@@ -27,13 +55,66 @@ public class GraphSteps {
 	}
 
 	@When("The user clicks the Case Details button")
-	public void the_user_clicks_the_case_details_button() {
+	public void the_user_clicks_the_case_details_button(String OptionOnGraph) {
+		graphpage.clickGraphOptions(OptionOnGraph);
+
+	}
+	
+	@When("The user clicks {string} link on the Graph page")
+	public void the_user_clicks_link_on_the_graph_page(String OptionOnGraph) {
+		graphpage.clickGraphOptions(OptionOnGraph);
+
+		}
+
+	
+	@When("run invalid code by clicking run button after entering {string} in try editor for graph module")
+	public void run_invalid_code_by_clicking_run_button_after_entering_in_try_editor_for_graph_module(String code) {
+	    graphpage.codeEditorSendKeys(code);
+		graphpage.clickRunBtn();
+	}
+	
+	@Then("The user should see alert with  error message {string} on Graph Module in the code page")
+	public void the_user_should_see_alert_with_error_message_on_graph_module_in_the_code_page(String expectedMessage) {
+		String actualMessage = graphpage.getAlertMessageAndAccept();
+		Assert.assertEquals(actualMessage.trim(), expectedMessage.trim(), "Alert message mismatch!");
+
+	}
+	
+	
+	@When("run valid code by clicking run button after entering {string} in try editor for graph module")
+	public void run_valid_code_by_clicking_run_button_after_entering_in_try_editor_for_graph_module(String code) {
+		graphpage.codeEditorSendKeys(code);
+		graphpage.clickRunBtn();
+	}
+
+	@Then("The user should able to see {string} output in the console on Graph Module in the code page")
+	public void the_user_should_able_to_see_output_in_the_console_on_graph_module_in_the_code_page(String string) {
+		 graphpage.outputDisplayed();
+	}
+
+	@When("The user write the valid code in Editor and click the {string} Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_button(String string) {
 	}
 
 	@Then("The user should be redirected to {string} page")
 	public void the_user_should_be_redirected_to_page(String string) {
 	}
+	
 
+	@When("The user clicks {string} button on Graph page")
+	public void the_user_clicks_button_on_graph_page(String OptionOnGraph) {
+		graphpage.clickGraphOptions(OptionOnGraph);
+
+	}
+	
+		@When("The user clicks {string} button on the Graph page")
+	    public void the_user_clicks_button_on_the_graph_page(String OptionOnGraph) {
+		graphpage.clickGraphOptions(OptionOnGraph);
+
+	}
+
+
+	
 	@When("The user clicks {string} button in Graphs in Python page")
 	public void the_user_clicks_button_in_graphs_in_python_page(String string) {
 	}
@@ -42,23 +123,39 @@ public class GraphSteps {
 	public void the_user_should_be_redirected_to_a_page_having_an_editor_with_a_button(String string) {
 	}
 
-	@Given("The user is in the tryEditor page")
+	/*@Given("The user is in the tryEditor page")
 	public void the_user_is_in_the_try_editor_page() {
-	}
+	}*/
+	
+	
 
-	@When("The user clicks the {string} button without entering the code in the Editor")
-	public void the_user_clicks_the_button_without_entering_the_code_in_the_editor(String string) {
-	}
+
 
 	@When("The user write the invalid code in Editor and click the {string} Button")
 	public void the_user_writes_the_invalid_code_in_editor_and_click_the_button(String string) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
-
-	@When("The user write the valid code in Editor and click the {string} Button")
-	public void the_user_write_the_valid_code_in_editor_and_click_the_button(String string) {
+	
+	@When("The user clicks {string} link on Graph page")
+	public void the_user_clicks_link_on_graph_page(String OptionOnGraph) {
+		graphpage.clickGraphOptions(OptionOnGraph);
 	}
+	
+
+
+	@Given("The user is on the DSAlgo Graph page")
+	public void the_user_is_on_the_ds_algo_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user sees {string} label on the {string} page")
+	public void the_user_sees_label_on_the_page(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
 
 	@Then("The user should able to see output in the console with no error message")
 	public void the_user_should_able_to_see_output_in_the_console_with_no_error_message() {
--- src/test/resources/features/graph.feature
+++ src/test/resources/features/graph.feature
@@ -1,84 +1,42 @@
-Feature: Validate Graph Module
+Feature: Graph Page Functionality
+  As a logged in user I should be able to access and use the Graph module
 
-Background: The user sign in to dsAlgo Portal
+Background: 
+    Given The user is on the DSAlgo portal
+    When The user clicks "GetStarted" button on welcome page
+    And The user logs in with valid username "qatitans1" and password "1@Chicago"
+    Then The user lands on the home page of DSAlgo portal
 
-#Graph1
-Scenario: Verify that user is able to navigate to "Graph" data structure page
-Given The user is on the Home page after Sign in
-When The user clicks the "Getting Started" button in Graph Panel 
-Then The user be directed to "Graph" Data Structure Page
+Scenario: User navigates to the "Graph" Data Structure page from the home page
+    When user clicks "Getting Started" button in Graph Panel
+    Then The user lands on the "Graph" page
+    
+Scenario Outline: Verify that user sees error message on entering invalid code in "try me" editor for "Graph Representations" page
+    Given The user is on Graph Data Structure
+    When The user clicks "<OptionOnGraph>" link on the Graph page
+    And The user clicks "Try Here" button
+    And run invalid code by clicking run button after entering "<code>" in try editor for graph module
+    Then The user should see alert with  error message "<errorMessage>" on Graph Module in the code page
 
-#Graph2
-Scenario: Verify that user is able to navigate to "Graph" data structure page
-Given The user is on the Home page after Sign in
-When The user select Graph item from the drop down
-Then The user be directed to "Graph" Data Structure Page
+    Examples: 
+      | OptionOnGraph           | code | errorMessage                                    |
+      | Graph				    | abcd | NameError: name 'abcd' is not defined on line 1 |
+      | Graph Representations	| abcd | NameError: name 'abcd' is not defined on line 1 |
+      
+  Scenario Outline: Verify that user is able to see output for valid python code for "Graph Representations" page
+    Given The user is on Graph Data Structure
+    When The user clicks "<OptionOnGraph>" link on Graph page
+    And The user clicks "Try Here" button
+    And run valid code by clicking run button after entering "<code>" in try editor for graph module
+    Then The user should able to see "<message>" output in the console on Graph Module in the code page
 
-#Graph3
-Scenario: Verify that user is able to navigate to "Graphs" page
-Given The user is in the Assessments Page
-When The user clicks the Case Details button
-Then The user should be redirected to "Graphs" page
-
-#Graph4
-Scenario: Verify that user is able to navigate to "try here>>>" page for "Graph" page
-Given The user is on the "Graphs" page
-When The user clicks "Try Here>>>" button in Graphs in Python page
-Then The user should be redirected to a page having an Editor with a "Run" button
-
-@Graph5
-Scenario: Verify that user receives error when click on "Run" button without entering code for "Graph" page
-Given The user is in the tryEditor page
-When The user clicks the "Run" button without entering the code in the Editor
-Then The user should able to see an error message in alert window
-
-@Graph6
-Scenario: Verify that user receives error for invalid python code for "Graph" page
-Given The user is in the tryEditor page
-When The user write the invalid code in Editor and click the "Run" Button
-Then The user should able to see an error message in alert window
-
-@Graph7
-Scenario: Verify that user is able to see output for valid python code for "Graph" page
-Given The user is in the tryEditor page
-When The user write the valid code in Editor and click the "Run" Button
-Then The user should able to see output in the console with no error message
-
-@Graph8
-Scenario: Verify that user is able to navigate to "Practice Questions" Page for "Graph" page
-Given The user is in the "Graph" page after Sign in
-When The user clicks "Practice Questions" link in the left navigation bar
-Then The user should be redirected to "Practice Questions" page
-
-@Graph9
-Scenario: Verify that user is able to navigate to "Graph Representations" page from "Graph" page
-Given The user is on the "Graph" page
-When The user clicks the "Graph Representation" link in the left navigation bar
-Then The user should be redirected to "Graph Represntations" page, and try Editor with "Run" button
-
-@Graph10
-Scenario: Verify that user receives error for invalid python code on running "Graph Representations" page
-Given The user is on the practice question editor
-When The user writes the invalid code in Editor and Click the Run Button
-Then The user should able to see an error message in alert window
-
-@Graph11
-Scenario: Verify that user is able to run valid python code for "Graph Representations" page
-Given The user is on the practice question editor
-When The user write the valid code in Editor and Click the Run Button
-Then The user should able to see output in the console
-
-@Graph12
-Scenario: Verify that user receives error on submitting invalid python code for "Graph Representations" page
-Given The user is on the practice question editor
-When The user write the invalid code in Editor and Click the "Run" Button
-Then The user see an error message "Error occurred during submission"
-
-@Graph13
-Scenario: Verify that user receives error on submitting invalid python code for "Graph Representations" page
-Given The user is on the practice question editor
-When The user write the invalid code in Editor and Clicks "Run" Button
-Then The user see output with no error message
-
-
-
+    Examples: 
+      | OptionOnGraph		   	  | code                     | message         |
+      | Graph			             | print('Hello QATitans!') | Hello QATitans! |
+      | Graph Representations   | print('Hello QATitans!') | Hello QATitans! |
+    
+      
+ # Scenario: Verify that "Graph" label is displayed on the "Graph" page
+  #  Given The user is on the DSAlgo Graph page
+   # When The user is on the "Graph" page
+   # Then The user sees "Graph" label on the "Graph" page
